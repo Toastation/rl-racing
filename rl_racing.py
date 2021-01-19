745a109227652d269cb65fbdb82e823ec0a70574
Toastation <toastations@gmail.com>
2021-01-19 16:44:34 +0100

    main loop discretization

--- rl_racing.py
+++ rl_racing.py
@@ -8,16 +8,25 @@ init_state = env.reset()
 # Note: au début du jeu la caméra est loin du terrain et zoom progressivement pendant ~1 seconde
 # faudrait peut être ne pas faire d'observation sur cette période là
 
+NB_FRAME_SKIP = 4
+
 tot_reward = 0
 frame_count = 0
+
 while True:
     env.render()
-    state, reward, finish, _ = env.step(env.action_space.sample()) # random action
+
+    reward_cum = 0 # we accumulate the reward of the skipped frames
+    for _ in range(NB_FRAME_SKIP + 1): # +1 is the action that counts
+        state, reward, finish, _ = env.step(env.action_space.sample()) # random action
+        reward_cum += reward
+
+    tot_reward += reward_cum
     processed_state = process_image(state)
     # if frame_count == 120:
     #     cv2.imshow("hello", processed_state)
     #     cv2.waitKey(0)
-    tot_reward += reward
+    
     frame_count += 1
     if finish:
         print(f"Frames: {frame_count}   |   Total reward: {tot_reward}")
